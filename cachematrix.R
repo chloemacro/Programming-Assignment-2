68bdf65fcc2dfc6007042062ec7a0ba7d3425cdb
chloemacro <82433132+chloemacro@users.noreply.github.com>
2021-04-13 14:26:01 +0800

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,32 @@
+
+
+createMtrx <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set = set,
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
+}
+
+
+
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInv(inv)
+  inv
+}
+
